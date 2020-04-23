d93ae78910e4af410c1d4050e48408f23dad9ba9
Mark Molinaro <mamolin@microsoft.com>
2020-04-23 12:40:44 -0700

    fix typings issue

--- execr.ts
+++ execr.ts
@@ -2,6 +2,28 @@ import spawn from "cross-spawn";
 import { memoize } from "lodash";
 import { SpawnOptions } from "child_process";
 
+type AnyKey<T> = T & {
+  [key: string]: T
+}
+
+type Tail<T extends any[]> = 
+  ((...args: T)=>void) extends ((firstArg: any, ...restOfArgs: infer R)=>void) ? R : never;
+
+type TailParameters<T extends (...args: any) => any> = Tail<Parameters<T>>
+
+// There must be a better way...
+type FunctionWithArbitraryParameters<T extends (this: AnyKey<T>, ...args: any) => any> = 
+  T & 
+  AnyKey<T> & 
+  AnyKey<AnyKey<T>> &
+  AnyKey<AnyKey<AnyKey<T>>> & 
+  AnyKey<AnyKey<AnyKey<AnyKey<T>>>> & 
+  AnyKey<AnyKey<AnyKey<AnyKey<AnyKey<T>>>>> & 
+  AnyKey<AnyKey<AnyKey<AnyKey<AnyKey<AnyKey<T>>>>>> &
+  AnyKey<AnyKey<AnyKey<AnyKey<AnyKey<AnyKey<AnyKey<T>>>>>>> &
+  AnyKey<AnyKey<AnyKey<AnyKey<AnyKey<AnyKey<AnyKey<AnyKey<T>>>>>>>>;
+
+
 
 const memoizeSpawnSync = memoize(spawn.sync, (...args) => JSON.stringify(args));
 
